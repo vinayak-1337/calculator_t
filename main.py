492d0ad2be44a5a5a8398fafb5564c718fcc25bb
vinayak <vinayak@gkmit.co>
2022-10-04 12:48:07 +0530

    added subtraction feature

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 
-def multiply(x, y):
-    return x * y
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print(num1, "*", num2, "=", multiply(num1, num2))
+# def multiply(x, y):
+#     return x * y
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# print(num1, "*", num2, "=", multiply(num1, num2))
 
 def sub(a,b):
     return a-b
 
 
-def add():
-    inp1 = int(input("Enter first number -> "))
-    inp2 = int(input("Enter second number -> "))
-    result = inp1 + inp2
-    print("Result of ",inp1,"+",inp2,'=', result)
+# def add():
+#     inp1 = int(input("Enter first number -> "))
+#     inp2 = int(input("Enter second number -> "))
+#     result = inp1 + inp2
+#     print("Result of ",inp1,"+",inp2,'=', result)
 
-def div():
-    a= int(input("Enter first number -> "))
-    b = int(input("Enter second number -> "))
-    divide=a/b
-    return divide
+# def div():
+#     a= int(input("Enter first number -> "))
+#     b = int(input("Enter second number -> "))
+#     divide=a/b
+#     return divide
 
 
 
-add()
+# add()
 
 sub()
 
-multiply()
+# multiply()
 
-div()
+# div()

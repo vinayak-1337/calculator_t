ab49c57b2d75636a56057090180fcd5093872646
vinayak <vinayak@gkmit.co>
2022-10-04 12:49:52 +0530

    added division feature

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 
-def multiply(x, y):
-    return x * y
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print(num1, "*", num2, "=", multiply(num1, num2))
+# def multiply(x, y):
+#     return x * y
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# print(num1, "*", num2, "=", multiply(num1, num2))
 
-def sub(a,b):
-    return a-b
+# def sub(a,b):
+#     return a-b
 
 
-def add():
-    inp1 = int(input("Enter first number -> "))
-    inp2 = int(input("Enter second number -> "))
-    result = inp1 + inp2
-    print("Result of ",inp1,"+",inp2,'=', result)
+# def add():
+#     inp1 = int(input("Enter first number -> "))
+#     inp2 = int(input("Enter second number -> "))
+#     result = inp1 + inp2
+#     print("Result of ",inp1,"+",inp2,'=', result)
 
 def div():
     a= int(input("Enter first number -> "))
@@ -23,10 +23,10 @@ def div():
 
 
 
-add()
+# add()
 
-sub()
+# sub()
 
-multiply()
+# multiply()
 
 div()
